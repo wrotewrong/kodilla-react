58c1b7d1ef762d97cdb48d2bae998d552829b45c
Andrzej Popiolek <xandrzejpopiolek@gmail.com>
2022-10-09 18:40:01 +0200

    Add global styles

---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,12 @@
+const Hero = () => {
+  return (
+    <div className='hero'>
+      <h1 className='title'>My first React App</h1>
+      <p className='subtitle'>
+        A simple to-do app, with lists, columns and card
+      </p>
+    </div>
+  );
+};
+
+export default Hero;
