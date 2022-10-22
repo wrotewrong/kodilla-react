9dc39057ffcc0f3971778bdff40da863ea9e3c07
Andrzej Popiolek <xandrzejpopiolek@gmail.com>
2022-10-22 18:56:43 +0200

    Add Redux to the project

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -1,8 +1,12 @@
 import styles from './Column.module.scss';
 import Card from '../Card/Card';
 import CardForm from '../CardForm/CardForm';
+import { useSelector } from 'react-redux';
 
 const Column = (props) => {
+  const cards = useSelector((state) =>
+    state.cards.filter((card) => card.columnId === props.id)
+  );
   return (
     <article className={styles.column}>
       <h2 className={styles.title}>
@@ -10,7 +14,7 @@ const Column = (props) => {
         {props.title}
       </h2>
       <ul className={styles.cards}>
-        {props.cards.map((card) => (
+        {cards.map((card) => (
           <Card key={card.id} title={card.title} />
         ))}
       </ul>
--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,70 +1,72 @@
 import styles from './List.module.scss';
 import Column from '../Column/Column';
-import { useState } from 'react';
-import shortid from 'shortid';
+// import { useState } from 'react';
+// import shortid from 'shortid';
 import ColumnForm from '../ColumnForm/ColumnForm';
+import { useSelector } from 'react-redux';
 
 const List = () => {
-  const [columns, setColumns] = useState([
-    {
-      id: 1,
-      title: 'Books',
-      icon: 'book',
-      cards: [
-        { id: 1, title: 'This is Going to Hurt' },
-        { id: 2, title: 'Interpreter of Maladies' },
-      ],
-    },
-    {
-      id: 2,
-      title: 'Movies',
-      icon: 'film',
-      cards: [
-        { id: 1, title: 'Harry Potter' },
-        { id: 2, title: 'Star Wars' },
-      ],
-    },
-    {
-      id: 3,
-      title: 'Games',
-      icon: 'gamepad',
-      cards: [
-        { id: 1, title: 'The Witcher' },
-        { id: 2, title: 'Skyrim' },
-      ],
-    },
-  ]);
+  const columns = useSelector((state) => state.columns);
+  // const [columns, setColumns] = useState([
+  //   {
+  //     id: 1,
+  //     title: 'Books',
+  //     icon: 'book',
+  //     cards: [
+  //       { id: 1, title: 'This is Going to Hurt' },
+  //       { id: 2, title: 'Interpreter of Maladies' },
+  //     ],
+  //   },
+  //   {
+  //     id: 2,
+  //     title: 'Movies',
+  //     icon: 'film',
+  //     cards: [
+  //       { id: 1, title: 'Harry Potter' },
+  //       { id: 2, title: 'Star Wars' },
+  //     ],
+  //   },
+  //   {
+  //     id: 3,
+  //     title: 'Games',
+  //     icon: 'gamepad',
+  //     cards: [
+  //       { id: 1, title: 'The Witcher' },
+  //       { id: 2, title: 'Skyrim' },
+  //     ],
+  //   },
+  // ]);
 
-  const addColumn = (newColumn) => {
-    setColumns([
-      ...columns,
-      {
-        id: shortid(),
-        title: newColumn.title,
-        icon: newColumn.icon,
-        cards: [],
-      },
-    ]);
-  };
+  // const addColumn = (newColumn) => {
+  //   setColumns([
+  //     ...columns,
+  //     {
+  //       id: shortid(),
+  //       title: newColumn.title,
+  //       icon: newColumn.icon,
+  //       cards: [],
+  //     },
+  //   ]);
+  // };
 
-  const addCard = (newCard, columnId) => {
-    const columnsUpdated = columns.map((column) => {
-      if (column.id === columnId)
-        return {
-          ...column,
-          cards: [...column.cards, { id: shortid(), title: newCard.title }],
-        };
-      else return column;
-    });
+  // const addCard = (newCard, columnId) => {
+  //   const columnsUpdated = columns.map((column) => {
+  //     if (column.id === columnId)
+  //       return {
+  //         ...column,
+  //         cards: [...column.cards, { id: shortid(), title: newCard.title }],
+  //       };
+  //     else return column;
+  //   });
 
-    setColumns(columnsUpdated);
-  };
+  //   setColumns(columnsUpdated);
+  // };
 
   return (
     <div className={styles.list}>
       <header className={styles.header}>
         <h2 className={styles.title}>
-          Things to do <span>soon</span>
+          Things to do<span>soon!</span>
         </h2>
       </header>
       <p className={styles.description}>
@@ -72,17 +74,10 @@ const List = () => {
       </p>
       <section className={styles.columns}>
         {columns.map((column) => (
-          <Column
-            key={column.id}
-            id={column.id}
-            title={column.title}
-            icon={column.icon}
-            cards={column.cards}
-            action={addCard}
-          />
+          <Column key={column.id} {...column} />
         ))}
       </section>
-      <ColumnForm action={addColumn} />
+      <ColumnForm />
     </div>
   );
 };
